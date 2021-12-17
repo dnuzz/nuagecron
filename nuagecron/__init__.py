fb426b3c2b3c650032f422f7193c735851c1c84b
dnuzz <dnuzzomueller@gmail.com>
2021-12-17 17:39:44 -0500

    A few more structural fixes

--- nuagecron/__init__.py
+++ nuagecron/__init__.py
@@ -1,3 +1,3 @@
-from os import path
+from os import path, environ
 import json
-SERVICE_NAME = json.load(open(path.join(path.dirname(__file__),'servicename.json')))['servicename']
+SERVICE_NAME = environ.get("NUAGECRON_SERVICE_NAME" ,json.load(open(path.join(path.dirname(__file__),'servicename.json')))['servicename'])
