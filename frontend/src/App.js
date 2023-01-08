f7d33460b58b4958b56bdeb1e0a4a67c852b8924
dnuzz <dnuzzomueller@gmail.com>
2023-01-07 22:39:56 -0500

    Frontend Auth0

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,24 +7,47 @@ import {
   createBrowserRouter,
   RouterProvider,
   Route,
+  Outlet,
+  createRoutesFromElements,
 } from "react-router-dom";
 import ErrorPage from './views/ErrorPage'
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
+import Profile from './components/Profile';
+import { useAuth0 } from '@auth0/auth0-react';
+import { Auth0ProviderWithNavigate } from './auth0-with-navigate';
 
 export const api = new NuagecronApi('/api')
 const defaultMaterialTheme = createTheme();
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <ScheduleTable api={api}/>,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: "/schedule/:schedule_id",
-    element: <ScheduleView api={api}/>,
-    errorElement: <ErrorPage />
+
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<Auth0ProviderWithNavigate />} errorElement={<ErrorPage/>}>
+        <Route element={<Layout />}>
+          <Route path="/" element={<ScheduleTable api={api}/>} />
+          <Route path="/schedule/:schedule_id" element={<ScheduleView api={api}/>} />
+        </Route>
+    </Route>
+));
+
+function Layout() {
+
+  const { isAuthenticated } = useAuth0()
+
+  let button;
+  if (isAuthenticated) {
+    button = <LogoutButton/>;
+  } else {
+    button = <LoginButton/>
   }
-]);
+
+  return <div>
+    <Profile/>
+    {button}
+    <Outlet />
+  </div>
+}
 
 export default function App() {
   
--- nuagecron/adapters/aws/adapters.py
+++ nuagecron/adapters/aws/adapters.py
@@ -188,7 +188,7 @@ class DynamoDbAdapter(BaseDBAdapter):
             Limit=count,
             KeyConditionExpression="schedule_id = :V",
             ExpressionAttributeValues={":V": {"S": schedule_id}},
-            ScanIndexForward=True,
+            ScanIndexForward=False,
             **addl_kwargs,
         )
         ret_val = [Execution(**dynamo_to_dict(item)) for item in response["Items"]]
@@ -200,7 +200,7 @@ class DynamoDbAdapter(BaseDBAdapter):
                 Limit=count,
                 KeyConditionExpression="schedule_id = :V",
                 ExpressionAttributeValues={":V": {"S": schedule_id}},
-                ScanIndexForward=True,
+                ScanIndexForward=False,
                 ExclusiveStartKey=response["LastEvaluatedKey"],
             )
             ret_val.extend(
